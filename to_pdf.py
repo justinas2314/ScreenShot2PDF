949d60f48c75f8ee59ad2d2fe7e7f232c9d25af0
Justinas Staskevicius <staskevicius14@gmail.com>
2021-01-10 21:44:29 +0200

    Update to_pdf.py

--- to_pdf.py
+++ to_pdf.py
@@ -1,8 +1,12 @@
 import glob
+try:
+    import ocrmypdf
+except (ImportError, ModuleNotFoundError):
+    ocrmypdf = None
 from PIL import Image
 
 
-def main(output_path: str, image_path: str = None, images: [Image] = None, func=lambda x: x):
+def main(output_path: str, image_path: str = None, images: [Image] = None, func=lambda x: x, ocr: bool = False):
     """
     Assumptions:
         Either `glob_path` or `images` is not None
@@ -19,7 +23,9 @@ def main(output_path: str, image_path: str = None, images: [Image] = None, func=
         ]
     base, *rest = map(func, images)
     base.save(output_path, resolution=100., save_all=True, append_images=rest)
+    if ocr and ocrmypdf is not None:
+        ocrmypdf.ocr(output_path, output_path)
 
 
 if __name__ == '__main__':
-    main(r'data\output.pdf', r'data\images', func=lambda x: x.crop((0, 0, 1600, 1080)))
+    main(r'data\new_test_output.pdf', r'data\images', func=lambda x: x.crop((0, 0, 1600, 1080)), ocr=True)
